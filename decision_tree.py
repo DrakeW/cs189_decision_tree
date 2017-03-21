86f86226db0971fe480569cb5c7554e23a69ed8f
DrakeW <junyuw@berkeley.edu>
2017-03-21 14:55:32 -0700

    using self made entropy function & speed up calc

--- decision_tree.py
+++ decision_tree.py
@@ -37,7 +37,7 @@ class DecisionTree:
     def build_tree(self, data, labels):
         if len(labels) == 0:
             return None
-        if len(np.unique(labels)) == 1:
+        if len(set(labels)) == 1:
             return DTNode(None, None, None, None, labels[0])
         split_rule = self.segmenter(data, labels)
         if split_rule == None:
@@ -55,24 +55,29 @@ class DecisionTree:
         best_split_rule, smallest_impurity = None, float("inf")
         for feat in range(data.shape[1]):
             # TODO: use radix sort if that can make it faster
-            values = np.sort(np.unique(data[:,feat]))
+            values = np.sort(list(set(data[:,feat])))
             for idx, val in enumerate(values):
                 if idx == len(values) - 1:
                     continue
                 split_val = float(values[idx] + values[idx+1])/2
                 left = labels[data[:,feat] < split_val]
                 right = labels[data[:, feat] >= split_val]
-                imp = self.impurity(stats.itemfreq(left)[:,1], stats.itemfreq(right)[:,1])
+                imp = self.impurity(self.get_hist(left), self.get_hist(right))
                 if imp < smallest_impurity:
                     smallest_impurity = imp
                     best_split_rule = (feat, split_val)
         return best_split_rule
 
+    def get_hist(self, labels):
+        return np.unique(labels, return_counts=True)[1] / float(len(labels))
+
     """
     return the badness (to minimize) of a split
     """
     def impurity(self, left_label_hist, right_label_hist):
-        H_l, H_r = stats.entropy(left_label_hist), stats.entropy(right_label_hist)
+        H_l, H_r = self.entropy(left_label_hist), self.entropy(right_label_hist)
         len_l, len_r = len(left_label_hist), len(right_label_hist)
         return (len_l * H_l + len_r * H_r) / (len_l + len_r)
 
+    def entropy(self, freq_data):
+        return -1 * np.sum(freq_data * np.log2(freq_data))
